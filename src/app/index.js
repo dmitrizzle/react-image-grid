d7554d306526deb7f74aad905d94e4b39861ede8
Dmitri <d@eurekaking.com>
2018-08-03 20:36:36 -0700

    Add babel transform class props for arrow functions & further mock

--- src/app/index.js
+++ src/app/index.js
@@ -9,20 +9,35 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Picture from "./components/Picture";
 
-const mockSrc =
-  "https://res.cloudinary.com/analog-cafe/image/upload/c_scale,fl_progressive,w_520/image-froth_1333333_Byl1aAiE7.jpg";
-export default () =>
-  <ThemeProvider theme={Sugar({})}>
-    <div>
-      <Header />
-      <Main>
-        <Picture src={mockSrc} />
-        <Picture src={mockSrc} />
-        <Picture src={mockSrc} />
-        <Picture src={mockSrc} />
-        <Picture src={mockSrc} />
-        <Picture src={mockSrc} />
-        <Picture src={mockSrc} />
-      </Main>
-    </div>
-  </ThemeProvider>;
+const mockSrc = [
+  "https://farm2.staticflickr.com/1818/43766087152_a5f8bfe93a_m.jpg",
+  "https://farm1.staticflickr.com/939/43098220234_44b423bdaa_m.jpg",
+  "https://farm2.staticflickr.com/1819/43098024864_b6490c694f_m.jpg",
+  "https://farm2.staticflickr.com/1778/42005642750_64ddc48dd5_m.jpg",
+  "https://farm2.staticflickr.com/1840/42910771235_e28d308181_m.jpg",
+  "https://farm2.staticflickr.com/1834/43814235591_c33d2a996e_m.jpg",
+  "https://farm1.staticflickr.com/856/28855487417_25a7876483_m.jpg",
+  "https://farm2.staticflickr.com/1813/43743914412_51215db1b1_m.jpg"
+];
+export default class extends React.PureComponent {
+  constructor(props) {
+    super()
+  }
+  componentDidMount = () => {
+    console.log("mount");
+  }
+  render = () => {
+    return (
+      <ThemeProvider theme={Sugar({})}>
+        <div>
+          <Header />
+          <Main>
+            {mockSrc.map(item => {
+              return <Picture src={item} key={item} />;
+            })}
+          </Main>
+        </div>
+      </ThemeProvider>
+    );
+  };
+}
