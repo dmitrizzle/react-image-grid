e53cd0e4cd092e00b688941fe9a633537c5f8429
Dmitri <d@eurekaking.com>
2018-08-03 21:31:52 -0700

    Integrate continous loading

--- src/app/index.js
+++ src/app/index.js
@@ -4,13 +4,13 @@ import "typeface-indie-flower/index.css";
 import { Sugar } from "@roast-cms/react-sugar-styled";
 import { ThemeProvider } from "styled-components";
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Picture from "./components/Picture";
 
-const DEFAULT_GRID_STATE = [
+const DEFAULT_GALLERY_DATA = [
   {
     media: {}
   },
@@ -25,23 +25,49 @@ export default class extends React.PureComponent {
   constructor(props) {
     super();
     this.state = {
-      gallery: DEFAULT_GRID_STATE
+      itemsPerPage: 12,
+      gallery: {
+        data: DEFAULT_GALLERY_DATA,
+        page: 1,
+        isLastPage: false
+      }
     };
+    this.handleMore = this.handleMore.bind(this);
   }
-  componentDidMount = () => {
+  fetch = page => {
     axios({
       method: "get",
       params: {
-        _page: 1,
-        _limit: 12
+        _page: page,
+        _limit: this.state.itemsPerPage
       },
       url: "http://localhost:3000/posts"
     }).then(response => {
+      const isLastPage = !(
+        response.data.length - this.state.itemsPerPage ===
+        0
+      );
       this.setState({
-        gallery: response.data
-      })
-      console.log(response.data);
+        gallery: {
+          page,
+          isLastPage,
+          data: [...this.state.gallery.data, ...response.data]
+        }
+      });
     });
+  };
+  componentDidMount = () => {
+    this.setState({
+      gallery: {
+        ...this.state.gallery,
+        data: []
+      }
+    });
+    this.fetch(this.state.gallery.page);
+  };
+  handleMore = event => {
+    event.preventDefault();
+    this.fetch(this.state.gallery.page + 1);
   };
   render = () => {
     return (
@@ -49,10 +75,15 @@ export default class extends React.PureComponent {
         <div>
           <Header />
           <Main>
-            {this.state.gallery.map(item => {
+            {this.state.gallery.data.map(item => {
               return <Picture src={item.media.m} key={Math.random()} />;
             })}
           </Main>
+          {this.state.gallery.data.length >= this.state.itemsPerPage &&
+            !this.state.gallery.isLastPage &&
+            <a href="#more" onClick={this.handleMore}>
+              More
+            </a>}
         </div>
       </ThemeProvider>
     );
